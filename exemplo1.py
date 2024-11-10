212f8d20b37f57b7d8052bb49b43394cde487967
Ruivo0210 <joaolucasr0210@gmail.com>
2024-11-09 23:01:16 -0300

    Versao com parametros

--- exemplo1.py
+++ exemplo1.py
@@ -5,7 +5,7 @@ classificador = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.
 imagem = cv2.imread('pessoas/beatles.jpg')
 imagemcinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
-facesDetectadas = classificador.detectMultiScale(imagemcinza) #Este comando serve para fazermos a detecção na imagem baseado naquilo que o classificador é treinado.
+facesDetectadas = classificador.detectMultiScale(imagemcinza, scaleFactor=1.1, minNeighbors = 8, minSize=(30,30)) #Este comando serve para fazermos a detecção na imagem baseado naquilo que o classificador é treinado.
 print(len(facesDetectadas)) #Exibe a quantidade de itens detectados na imagem
 print(facesDetectadas) #Exibe uma matriz de 4 valores identificando os pontos que formam um quadrado demarcando os itens encontrados
 
