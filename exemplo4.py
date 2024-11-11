ff6d1ed75f41a044e89209b6c33664d333fc5a4b
Ruivo0210 <joaolucasr0210@gmail.com>
2024-11-10 23:50:50 -0300

    adicionando identificador de tamanho de veiculo

--- exemplo4.py
+++ exemplo4.py
@@ -1,17 +1,19 @@
 import cv2
 
-classificador = cv2.CascadeClassifier('cascades/cars.xml') #Aqui, atribuimos uma variável para o classificador XML. Este está treinado para reconhecer rostos.
+classificador = cv2.CascadeClassifier('cascades\cars.xml') 
 
-imagem = cv2.imread('outros/carro1.jpg')
+imagem = cv2.imread('outros/carro3.jpg')
 imagemcinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
-facesDetectadas = classificador.detectMultiScale(imagemcinza, scaleFactor=1.1, minNeighbors = 8, minSize=(30,30)) #Este comando serve para fazermos a detecção na imagem baseado naquilo que o classificador é treinado.
-print(len(facesDetectadas)) #Exibe a quantidade de itens detectados na imagem
-print(facesDetectadas) #Exibe uma matriz de 4 valores identificando os pontos que formam um quadrado demarcando os itens encontrados
+facesDetectadas = classificador.detectMultiScale(imagemcinza, scaleFactor=1.01, minNeighbors = 4, minSize=(20,20))
+print(len(facesDetectadas))
+print(facesDetectadas)
 
 for (x,y,l,a) in facesDetectadas:
     print(x,y,l,a)
-    cv2.rectangle(imagem, (x,y), (x+l, y+a), (0,0,255),2) #Este comando serve para demarcarmos um triangulo dentro dos itens detectados na imagem e visualizarmos ela. Atribuimos a imagem, as coordenadas do quadrado, a cor dele e a largura.
+    cv2.rectangle(imagem, (x,y), (x+l, y+a), (0,0,255),2)
+    if (l >50 or a> 50):
+        print ("Fora do padrão")
 
-cv2.imshow("Carros encontrados", imagem)
+cv2.imshow("carros encontrados", imagem)
 cv2.waitKey()
